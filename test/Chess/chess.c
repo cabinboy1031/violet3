5c96d54ff1e2563c4fa25a1fce88b6ce044f3590
cabinboy1031 <enchantingMenace@gmail.com>
2022-12-02 22:40:16 -0800

    Small updates, making snek first.

--- test/Chess/chess.c
+++ test/Chess/chess.c
@@ -4,21 +4,27 @@
 #include <raylib.h>
 #include "grid.h"
 
+typedef struct Velocity{
+    int x,y;
+}Velocity;
+
+void MoveSnake(ecs_iter_t it){
+}
+
 int main(){
     VERPSetupRenderer(800, 800, "Test window!");
 
     VECSInit();
     VERPSetup();
     
-    SetGrid(80, 40,40);
-    
-    for(int x = 0; x <= 8; x++){
-        for(int y = 0; y <= 8; y++){
-            Color color;
-            if((x+y)%2 == 0){color = BLACK;} else {color = RED;}
-            CreateTile((Transform){{x,y},{},{1,1}},color);
-        }
-    }
+    SetGrid(20, 40,40);
+
+    ECS_COMPONENT(VECSGetWorld(), Color);
+    ecs_entity_t snake = CreateTile((Transform){{10,10,0},{},{1,1}},RED);
+    // Add a snake tag to this rectangle
+    ECS_COMPONENT(VECSGetWorld(),Velocity);
+    ecs_add(VECSGetWorld(),snake,Velocity);
+
     
     // Run the program
     while(VECSProgress(!WindowShouldClose()));
